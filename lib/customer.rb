92adcd3e54b6304754573badd08ced43144ce911
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-28 17:46:34 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,30 @@
 class Customer
-  
+    attr_accessor :name, :age
+    @@all = []
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self 
+    end 
+
+    def self.all
+        @@all 
+    end 
+
+    # Custom Constructor - allows us to create new meals as a customer
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+    end
+    
+    # Look at all meals and select only the one's belonging to this customer(self)
+    def meals
+        Meal.all.select do |meal|
+            meal.customer == self
+        end 
+    end
+    
+    # Get all the waiters this customer has interacted with by using the array of all their meals
+    def waiters
+        self.meals.map {|meal| meal.waiter}
+    end 
 end
--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,18 @@
+# We set up the Meal class as a "joining" model between our Waiter and Customer classes
 class Meal
+    attr_accessor :waiter, :customer, :total, :tip
 
+    @@all = []
+
+    def initialize(waiter, customer, total, tip=0)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end 
 end
